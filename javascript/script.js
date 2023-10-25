2bfbfd5cbac4914150c2b2807cd6e8b51811bc66
LEVOLARD <lucaschlaepfer@hotmail.ch>
2023-10-25 16:04:40 +0200

    update

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,21 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+  const images = document.querySelectorAll('.image');
+  let currentIndex = 0;
+
+  function nextImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      updateSlider();
+  }
+
+  function prevImage() {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      updateSlider();
+  }
+
+  function updateSlider() {
+      const offset = -currentIndex * images[0].width; // Breite des aktuellen Bildes
+      document.getElementById('image-slider').style.transform = `translateX(${offset}px)`;
+  }
