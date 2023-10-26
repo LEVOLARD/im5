86b69e617a3dd004a6d94740a52d325ad5cf44e6
LEVOLARD <lucaschlaepfer@hotmail.ch>
2023-10-26 10:58:59 +0200

    update

--- javascript/script.js
+++ javascript/script.js
@@ -21,6 +21,25 @@ function myFunction() {
   }
 
   function updateSlider() {
-      const offset = -currentIndex * images[0].width; // Breite des aktuellen Bildes
+      images.forEach(image => image.classList.remove('active'));
+      images[currentIndex].classList.add('active');
+      const offset = -currentIndex * images[0].width;
       document.getElementById('image-slider').style.transform = `translateX(${offset}px)`;
   }
+
+  function zoomEffect() {
+      const activeImage = images[currentIndex];
+      activeImage.style.transform = 'scale(1.1)';
+      setTimeout(() => {
+          activeImage.style.transform = 'scale(1)';
+      }, 2500); // Zoom zurücksetzen nach 2,5 Sekunden
+  }
+
+  function autoChangeImage() {
+      setInterval(() => {
+          nextImage();
+          zoomEffect(); // Starte den Zoom-Effekt
+      }, 5000); // Automatischer Wechsel alle 5 Sekunden
+  }
+
+  autoChangeImage(); // Starte den automatischen Bildwechsel
