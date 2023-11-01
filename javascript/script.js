37abbad246c132b8308f29ed8b3b180c0836ec86
LEVOLARD <lucaschlaepfer@hotmail.ch>
2023-11-01 22:03:49 +0100

    examples

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,4 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+// IMAGE SWAP
 
   const images = document.querySelectorAll('.image');
   let currentIndex = 0;
@@ -43,3 +36,24 @@ function myFunction() {
   }
 
   autoChangeImage(); // Starte den automatischen Bildwechsel
+
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+//ACCORDION
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        panel.style.display = "none";
+        } else {
+        panel.style.display = "block";
+        }
+    });
+    }
